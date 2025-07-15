2adabcb208358fba23b51ad85cec9176f49f42e2
ZhangYing3314 <52391832+ZhangYing3314@users.noreply.github.com>
2025-07-16 01:32:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -288,4 +288,6 @@ def progress(search_id):
     return jsonify({'all_links': all_links})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
